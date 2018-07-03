85bbfb6cd240d3ca951a5de8614aea548a4a214c
Sény Mbaye <senyr9@gmail.com>
2018-07-03 16:00:45 +0000

    premier commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PonyComponent } from './pony/pony.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PonyComponent
   ],
   imports: [
     BrowserModule
